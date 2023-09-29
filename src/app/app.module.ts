a4329dc8c45c6fed968cbf2ce3ea22834fced403
Max-munene <munenemaxwell2@gmail.com>
2023-09-29 10:19:06 +0300

    firestore-config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { environment } from 'src/environments/environment.prod';
+// import { AngularFireModule } from '@angular/fire';
+
+import { environment } from 'src/environments/environment';
 // Firebase
 // import * as firebase from 'firebase';
 // import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+// import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+// import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { Firestore } from '@angular/fire/firestore';
+
+// import { Firestore } from 'firebase/firestore';
 // import { AngularFireAuthModule } from 'angularfire2/auth';
 
-// import {AngularFireModule}
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+// import { CategoriesComponent } from './categories/categories.component';
+
+// Forms
+import { FormsModule } from '@angular/forms';
 import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
@@ -24,15 +33,20 @@ import { CategoriesComponent } from './categories/categories.component';
     HeaderComponent,
     FooterComponent,
     DashboardComponent,
+    // CategoriesComponent,
     CategoriesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => getFirestore()),
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
-  providers: [],
+  providers: [
+    // provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    // provideFirestore(() => getFirestore()),
+    // AngularFirestore,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { FirebaseApp } from '@angular/fire/app';
+// import { Firestore } from '@angular/fire/firestore';
+import { provideFirestore } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent {
+  constructor(private afs: AngularFirestore) {}
 
+  // formData: any;
+  onSubmit(formData: any) {
+    let categoryData = {
+      category: formData.value.category,
+    };
+    this.afs.collection('categories').add(categoryData);
+  }
 }
