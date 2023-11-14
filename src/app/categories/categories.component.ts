99db6ba84854ee0b07e2b2f03b05c73f01d32867
Max-munene <munenemaxwell2@gmail.com>
2023-11-14 16:19:58 +0300

    Brim-dash

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { FirebaseApp } from '@angular/fire/app';
-// import { Firestore } from '@angular/fire/firestore';
+import { Firestore } from '@angular/fire/compat';
 import { provideFirestore } from '@angular/fire/firestore';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent {
-  constructor(private afs: AngularFirestore) {}
+  constructor(private afs: Firestore) {}
 
   // formData: any;
   onSubmit(formData: any) {
