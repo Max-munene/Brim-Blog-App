fc58a1137666145934beaa0f52ce2509db5b436b
Max-munene <munenemaxwell2@gmail.com>
2023-12-15 21:05:34 +0300

    firestore path-method

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -9,13 +9,51 @@ import { AngularFireModule } from '@angular/fire/compat';
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent {
-  constructor(private fs: AngularFirestore) {}
+  constructor(private afs: AngularFirestore) {}
 
-  // formData: any;
+  // Submit Form Handler
   onSubmit(formData: any) {
     let categoryData = {
       category: formData.value.category,
     };
-    this.fs.collection('categories').add(categoryData);
+    let subCategoryData = {
+      subcategory: 'subCategory1',
+    };
+    this.afs
+      .collection('categories')
+      .add(categoryData)
+      .then((docRef) => {
+        console.log('Category added with ID:', docRef);
+
+        //path method
+        // this.afs.doc(`categories/${docRef.id}`).collection('subcategory').add(subCategoryData)
+
+        this.afs
+          .collection('categories')
+          .doc(docRef.id)
+          .collection('subcategory')
+          .add(subCategoryData)
+          .then((docRef1) => {
+            console.log(docRef1);
+
+            //path method
+
+            // this.afs.doc(`categories/${docRef.id}/subcategory/${docRef1.id}`).collection('subsubcategory').add(subCategoryData)
+
+            this.afs
+              .collection('categories')
+              .doc(docRef.id)
+              .collection('subcategory')
+              .doc(docRef1.id)
+              .collection('subsubcategory')
+              .add(subCategoryData)
+              .then((docRef2) => {
+                console.log('subsubcaterogy added successfully', docRef2);
+              });
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
